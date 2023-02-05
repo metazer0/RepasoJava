2056b7cad73054d64ed7d2a2a00db06ea225f546
Julio David <isaacdvid15.96@outlook.com>
2023-02-05 16:08:57 -0600

    update

---
+++ src/LiquidContainer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LiquidContainer {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+        }
+    }
+}
